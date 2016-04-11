1964e5703b817bd7cb58b81cb6ec808077907093
Lawrence Vanderpool <lawrence.vanderpool@gmail.com>
2016-04-11 14:14:34 -0500

    car can rotate at least

--- gui.py
+++ gui.py
@@ -12,12 +12,21 @@ clock = pg.time.Clock()
 white = 255, 255, 255
 black = 0, 0, 0
 
+rotational_degree = 0
+
 while True:
     for event in pg.event.get():
         if event.type == pg.QUIT: sys.exit()
-        
+
     screen.fill(white)
+
     pg.draw.polygon(screen, black, inside_points, 2)
     pg.draw.polygon(screen, black, outside_points, 2)
-    pg.draw.rect(screen, black, pg.Rect(150, 320, 15, 8))
+
+    car = pg.Surface((15, 8))
+    car.fill(black)
+    pg.draw.rect(car, black, pg.Rect(0, 0, 15, 8), 1)
+    car.set_colorkey(white)
+    car = pg.transform.rotate(car, 90)
+    screen.blit(car, (150, 320, 15, 8))
     pg.display.flip()
