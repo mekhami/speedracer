c6a08c3e6cb580ffdff8ba364713b3ab47e83c23
Lawrence Vanderpool <lawrence.vanderpool@gmail.com>
2016-04-11 12:35:38 -0500

    indentation change

--- gui.py
+++ gui.py
@@ -7,6 +7,7 @@ pg.init()
 
 size = width, height = 500, 500
 screen = pg.display.set_mode(size)
+clock = pg.time.Clock()
 
 white = 255, 255, 255
 black = 0, 0, 0
@@ -15,8 +16,8 @@ while True:
     for event in pg.event.get():
         if event.type == pg.QUIT: sys.exit()
         
-        screen.fill(white)
-        pg.draw.polygon(screen, black, inside_points, 2)
-        pg.draw.polygon(screen, black, outside_points, 2)
-        pg.draw.rect(screen, black, pg.Rect(150, 320, 15, 8))
-        pg.display.flip()
+    screen.fill(white)
+    pg.draw.polygon(screen, black, inside_points, 2)
+    pg.draw.polygon(screen, black, outside_points, 2)
+    pg.draw.rect(screen, black, pg.Rect(150, 320, 15, 8))
+    pg.display.flip()
